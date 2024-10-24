a0a4a94fce63aa7721e8329c294aa7bcce2b7f48
Mauricio Campos <mauriciocampos119@gmail.com>
2024-10-24 10:11:46 -0600

    add arround service

--- src/main/java/com/mcm/customer/aop/AfterAspect.java
+++ src/main/java/com/mcm/customer/aop/AfterAspect.java
@@ -2,7 +2,9 @@ package com.mcm.customer.aop;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,29 @@ import org.springframework.stereotype.Component;
 public class AfterAspect {
 
     @After("execution(* com.mcm.customer.service.*.*(..))")
-    private void logAfterAspect(final JoinPoint joinPoint) {
+    private void logAfterAspect(final JoinPoint joinPoint) throws Throwable {
         log.info("Class: {}", joinPoint.getSignature().getDeclaringTypeName());
         log.info("Method: {}", joinPoint.getSignature().getName());
         log.info("Args: {}", joinPoint.getArgs());
+
+        //final Object object = joinPoint.proceed();
+
+        //log.info("Response: {}", object);
+
+    }
+
+    @Around("execution(* com.mcm.customer.service.*.*(..))")
+    private Object logAroundAspect(final ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("Class: {}", joinPoint.getSignature().getDeclaringTypeName());
+        log.info("Method: {}", joinPoint.getSignature().getName());
+        log.info("Args: {}", joinPoint.getArgs());
+
+        final Object object = joinPoint.proceed();
+
+        log.info("Response: {}", object);
+
+        return object;
+
     }
 
 }
